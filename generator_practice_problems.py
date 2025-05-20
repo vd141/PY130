9f96f35b3ee0c3051c4768b2775949048757a923
Victor Dinh <vd141@users.noreply.github.com>
2025-05-20 12:54:46 +0000

    first pass complete

--- generator_practice_problems.py
+++ generator_practice_problems.py
@@ -25,8 +25,35 @@ capitalized = (word.capitalize() for word in listostrings)
 '''
 question 4
 '''
-def gen_func(listofstrings):
-    generator = (mini.capitalize() for mini in listofstrings)
-    print(tuple(generator))
+# def gen_func(listofstrings):
+#     generator = (mini.capitalize() for mini in listofstrings)
+#     print(tuple(generator))
 
-gen_func(listostrings)
+# gen_func(listostrings)
+
+def capped_strings(listostrings):
+    for word in listostrings:
+        yield word.capitalize()
+
+# print(tuple(capped_strings(listostrings)))
+
+'''
+question 5
+'''
+def capped_strings_over_len5(listicle):
+    for word in listicle:
+        if len(word) >= 5:
+            yield word.capitalize()
+
+strings = ['four', 'score', 'and', 'seven', 'years', 'ago']
+# print(set(capped_strings_over_len5(strings)))
+
+'''
+question 6
+'''
+def capped_strings_under_len5(listicle):
+    for word in listicle:
+        if len(word) < 5:
+            yield word.capitalize()
+
+print(set(capped_strings_under_len5(strings)))
