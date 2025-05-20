1fd762a613ca412d9c99f92bdee4ad7c67a86674
Victor Dinh <vd141@users.noreply.github.com>
2025-05-20 18:46:45 +0000

    file input/output aux files

---
+++ file_examples.py
@@ -0,0 +1,33 @@
+file = open('example.txt', 'r')
+# content = file.read()
+
+# print(content)
+# print(repr(content))
+
+# content2 = file.readlines()
+# print(content2)
+
+# file.close()
+print(type(file))
+
+result = '_'
+while result != repr(''):
+    result = repr(file.readline())
+    print(result)
+
+file.close()
+
+# Don't use a REPL to run this code
+file = open('output.txt', 'w')
+file.write('Hello, world!\n')
+
+lines = ['First line\n', 'Second line\n']
+file.writelines(lines)
+
+file.close()
+
+# Don't use a REPL to run this code
+# file = open('output.txt', 'a')
+# file.write('Third line!\n')
+# file.write('Last line!\n')
+# file.close()
