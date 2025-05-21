c91bc69860a0e0352c8586fb18bd8466fcc1d956
Victor Dinh <vd141@users.noreply.github.com>
2025-05-21 16:07:47 +0000

    second pass

--- Higher_Order_Functions/question_1.py
+++ Higher_Order_Functions/question_1.py
@@ -1,10 +1,31 @@
+# def select(callback, iterable):
+#     # return_me = []
+#     # for item in iterable:
+#     #     if callback(item):
+#     #         return_me.append(item)
+#     # return return_me
+#     return [item for item in iterable if callback(item)]
+
+# numbers = [1, 2, 3, 4, 5]
+# colors = {'red', 'orange', 'yellow', 'green',
+#           'blue', 'indigo', 'violet'}
+
+# odd_numbers = select(lambda number: number % 2 != 0, numbers)
+# print(odd_numbers)            # [1, 3, 5]
+
+# large_numbers = select(lambda number: number >= 10, numbers)
+# print(large_numbers)          # []
+
+# small_numbers = select(lambda number: number < 10, numbers)
+# print(small_numbers)          # [1, 2, 3, 4, 5]
+
+# short_color_names = select(lambda color: len(color) <= 5, colors)
+# print(short_color_names)      # ['blue', 'red', 'green']
+# # The order of the colors may vary, but should include the
+# # indicated colors.
+
 def select(callback, iterable):
-    # return_me = []
-    # for item in iterable:
-    #     if callback(item):
-    #         return_me.append(item)
-    # return return_me
-    return [item for item in iterable if callback(item)]
+    return [object for object in iterable if callback(object)]
 
 numbers = [1, 2, 3, 4, 5]
 colors = {'red', 'orange', 'yellow', 'green',
--- Higher_Order_Functions/question_4.py
+++ Higher_Order_Functions/question_4.py
@@ -1,11 +1,21 @@
-def reduce(callback, iterable, starting):
-    product = starting
-    for number in iterable:
-        product = callback(number, product)
-    return product
+# def reduce(callback, iterable, starting):
+#     product = starting
+#     for number in iterable:
+#         product = callback(number, product)
+#     return product
 
-# compute the sum of squares of a list of numbers
-numbers = list(range(46))
-# first calculate squares, then call reduce on the squares
-sum_of_squares = reduce(lambda number, accum: number ** 2 + accum, numbers, 0)
-print(sum_of_squares == sum([number ** 2 for number in numbers]))
+# # compute the sum of squares of a list of numbers
+# numbers = list(range(46))
+# # first calculate squares, then call reduce on the squares
+# sum_of_squares = reduce(lambda number, accum: number ** 2 + accum, numbers, 0)
+# print(sum_of_squares == sum([number ** 2 for number in numbers]))
+
+def reduce(callback, iterable, starting):
+    accum = starting
+    for item in iterable:
+        accum = callback(item, accum)
+    return accum
+
+sum_sq = reduce(lambda item, accum: item ** 2 + accum, [1, 2, 3, 4, 5, 6], 0)
+print(sum_sq)
+# 1 + 4 + 9 + 16 + 25 + 36 = 91
