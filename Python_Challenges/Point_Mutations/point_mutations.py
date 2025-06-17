227886fa3a1b20b6573726cf85d16671ad83c8d8
Victor Dinh <vd141@users.noreply.github.com>
2025-06-17 14:28:08 +0000

    midday progress

--- Python_Challenges/Point_Mutations/point_mutations.py
+++ Python_Challenges/Point_Mutations/point_mutations.py
@@ -1,3 +1,29 @@
 '''
 calculate the hamming distance between two DNA strands
+
+the hamming distanace is only defined for sequences of equal length. 
+if you have two sequences of unequal length, you should compute the hamming
+distance over the shorter length
+
+DNA class takes a string when initialized
+
+calls a method called hamming_distance which takes another string and calculates
+the hamming distance
 '''
+
+class DNA:
+    def __init__(self, strand):
+        self._strand = strand
+    
+    def hamming_distance(self, other_strand):
+        hamming= 0
+        shorter = (self._strand 
+                   if len(self._strand) <= len(other_strand )
+                   else other_strand)
+        longer = (self._strand 
+                   if len(self._strand) > len(other_strand )
+                   else other_strand)
+        for index, nucleotide in enumerate(shorter):
+            if nucleotide != longer[index]:
+                hamming += 1
+        return hamming
--- Python_Challenges/Point_Mutations/test_point_mutations.py
+++ Python_Challenges/Point_Mutations/test_point_mutations.py
@@ -2,43 +2,43 @@ import unittest
 from point_mutations import DNA
 
 class TestDNAMutations(unittest.TestCase):
-    @unittest.skip
+    # @unittest.skip
     def test_no_difference_between_empty_strands(self):
         self.assertEqual(0, DNA("").hamming_distance(""))
 
-    @unittest.skip
+    # @unittest.skip
     def test_no_difference_between_identical_strands(self):
         self.assertEqual(0, DNA("GGACTGA").hamming_distance("GGACTGA"))
 
-    @unittest.skip
+    # @unittest.skip
     def test_complete_hamming_distance_in_small_strand(self):
         self.assertEqual(3, DNA("ACT").hamming_distance("GGA"))
 
-    @unittest.skip
+    # @unittest.skip
     def test_hamming_distance_in_off_by_one_strand(self):
         strand = "GGACGGATTCTGACCTGGACTAATTTTGGGG"
         distance = "AGGACGGATTCTGACCTGGACTAATTTTGGGG"
         self.assertEqual(19, DNA(strand).hamming_distance(distance))
 
-    @unittest.skip
+    # @unittest.skip
     def test_small_hamming_distance_in_middle_somewhere(self):
         self.assertEqual(1, DNA("GGACG").hamming_distance("GGTCG"))
 
-    @unittest.skip
+    # @unittest.skip
     def test_larger_distance(self):
         self.assertEqual(2, DNA("ACCAGGG").hamming_distance("ACTATGG"))
 
-    @unittest.skip
+    # @unittest.skip
     def test_ignores_extra_length_on_other_strand_when_longer(self):
         self.assertEqual(3, DNA("AAACTAGGGG").hamming_distance("AGGCTAGCGGTAGGAC"))
 
-    @unittest.skip
+    # @unittest.skip
     def test_ignores_extra_length_on_original_strand_when_longer(self):
         strand = "GACTACGGACAGGGTAGGGAAT"
         distance = "GACATCGCACACC"
         self.assertEqual(5, DNA(strand).hamming_distance(distance))
 
-    @unittest.skip
+    # @unittest.skip
     def test_does_not_actually_shorten_original_strand(self):
         dna = DNA("AGACAACAGCCAGCCGCCGGATT")
         self.assertEqual(1, dna.hamming_distance("AGGCAA"))
