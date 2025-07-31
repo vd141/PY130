c873568a8772df9755a5e58b4a983e8a84de14ab
vd141 <victor.dinh141@gmail.com>
2025-07-31 14:23:36 -0600

    second pass through PY130

--- todolist/todo.py
+++ todolist/todo.py
@@ -1,29 +1,37 @@
-class Todo:
-    IS_DONE = 'X'
-    IS_UNDONE = ' '
+'''
+requirements:
+each todo item has a state consisting of a title and a completed mark
+    both state attributes should be defined as properties
+    both properties should have a getter
+    only the completed mark should have a setter
+'''
 
+class Todo:
+    DONE = 'X'
+    NOT_DONE = ' '
     def __init__(self, title):
         self._title = title
         self._done = False
 
+    def __str__(self):
+        marker = Todo.DONE if self.done else Todo.NOT_DONE
+        return f'[{marker}] {self.title}'
+    
+    def __eq__(self, other):
+        if not isinstance(other,Todo):
+            raise NotImplementedError
+        
+        return (True if (other.title == self.title and other.done == self.done)
+                else False)
+
     @property
     def title(self):
         return self._title
-
+    
     @property
     def done(self):
         return self._done
-
+    
     @done.setter
-    def done(self, done):
-        self._done = done
-
-    def __str__(self):
-        marker = self.IS_DONE if self.done else self.IS_UNDONE
-        return f'[{marker}] {self.title}'
-
-    def __eq__(self, other):
-        if isinstance(other, Todo):
-            return self.title == other.title and self.done == other.done
-
-        return NotImplemented
+    def done(self, is_done):
+        self._done = is_done
