4ca243dde95dee41a2c93250df3b97a0cfeaf46f
vd141 <victor.dinh141@gmail.com>
2025-08-22 12:31:13 -0400

    midday progress 8/22

--- Python_Challenges/Series/series.py
+++ Python_Challenges/Series/series.py
@@ -1,3 +1,18 @@
 
 class Series():
-    pass
+    # series is a class that takes a string of a number as an input when initialized
+    # add each slice to a parent list and return that parent list
+    # has one public instance method caled slices
+    def __init__(self, slice_me):
+        self._slice_me = slice_me
+
+    def slices(self, slice_length):
+        if slice_length > len(self._slice_me):
+            raise ValueError('Slice exceeds length of input.')
+        parent = []
+        # there can be len(input)-slice_len + 1 slices
+        for starting_index in range(len(self._slice_me) - slice_length + 1):
+            listed = list(self._slice_me[starting_index: 
+                                          starting_index + slice_length])
+            parent.append([int(elem) for elem in listed])
+        return parent
--- Python_Challenges/Series/test_series.py
+++ Python_Challenges/Series/test_series.py
@@ -2,75 +2,75 @@ import unittest
 from series import Series
 
 class SeriesTest(unittest.TestCase):
-    @unittest.skip
+    # @unittest.skip
     def test_simple_slices_of_one(self):
         series = Series("01234")
         self.assertEqual([[0], [1], [2], [3], [4]], series.slices(1))
 
-    @unittest.skip
+    # @unittest.skip
     def test_simple_slices_of_one_again(self):
         series = Series("92834")
         self.assertEqual([[9], [2], [8], [3], [4]], series.slices(1))
 
-    @unittest.skip
+    # @unittest.skip
     def test_simple_slices_of_two(self):
         series = Series("01234")
         self.assertEqual([[0, 1], [1, 2], [2, 3], [3, 4]], series.slices(2))
 
-    @unittest.skip
+    # @unittest.skip
     def test_other_slices_of_two(self):
         series = Series("98273463")
         expected = [[9, 8], [8, 2], [2, 7], [7, 3], [3, 4], [4, 6], [6, 3]]
         self.assertEqual(expected, series.slices(2))
 
-    @unittest.skip
+    # @unittest.skip
     def test_simple_slices_of_two_again(self):
         series = Series("37103")
         self.assertEqual([[3, 7], [7, 1], [1, 0], [0, 3]], series.slices(2))
 
-    @unittest.skip
+    # @unittest.skip
     def test_simple_slices_of_three(self):
         series = Series("01234")
         self.assertEqual([[0, 1, 2], [1, 2, 3], [2, 3, 4]], series.slices(3))
 
-    @unittest.skip
+    # @unittest.skip
     def test_simple_slices_of_three_again(self):
         series = Series("31001")
         self.assertEqual([[3, 1, 0], [1, 0, 0], [0, 0, 1]], series.slices(3))
 
-    @unittest.skip
+    # @unittest.skip
     def test_other_slices_of_three(self):
         series = Series("982347")
         expected = [[9, 8, 2], [8, 2, 3], [2, 3, 4], [3, 4, 7]]
         self.assertEqual(expected, series.slices(3))
 
-    @unittest.skip
+    # @unittest.skip
     def test_simple_slices_of_four(self):
         series = Series("01234")
         self.assertEqual([[0, 1, 2, 3], [1, 2, 3, 4]], series.slices(4))
 
-    @unittest.skip
+    # @unittest.skip
     def test_simple_slices_of_four_again(self):
         series = Series("91274")
         self.assertEqual([[9, 1, 2, 7], [1, 2, 7, 4]], series.slices(4))
 
-    @unittest.skip
+    # @unittest.skip
     def test_simple_slices_of_five(self):
         series = Series("01234")
         self.assertEqual([[0, 1, 2, 3, 4]], series.slices(5))
 
-    @unittest.skip
+    # @unittest.skip
     def test_simple_slices_of_five_again(self):
         series = Series("81228")
         self.assertEqual([[8, 1, 2, 2, 8]], series.slices(5))
 
-    @unittest.skip
+    # @unittest.skip
     def test_simple_slice_that_blows_up(self):
         series = Series("01234")
         with self.assertRaises(ValueError):
             series.slices(6)
 
-    @unittest.skip
+    # @unittest.skip
     def test_more_complicated_slice_that_blows_up(self):
         slice_string = "01032987583"
         series = Series(slice_string)
